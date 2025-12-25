eb27299d90a24e4fb1003818205160bba91bc2cc
Cristian Jonatas <cristian.jonatas@outlook.com>
2025-12-25 01:46:11 -0300

    Configuração de swagger e outras dependencias

--- src/main/java/com/example/GestaoProdutosApi/GestaoProdutosApiApplication.java
+++ src/main/java/com/example/GestaoProdutosApi/GestaoProdutosApiApplication.java
@@ -1,9 +1,13 @@
-package com.example.demo;
+package com.example.GestaoProdutosApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		DataSourceAutoConfiguration.class
+})
+
 public class GestaoProdutosApiApplication {
 
 	public static void main(String[] args) {
